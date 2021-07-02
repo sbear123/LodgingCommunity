b9937524487a239df9be851ee973d1cda6a66702
sbear123 <sbear123@naver.com>
2021-07-02 15:10:25 +0900

    [ADD] VIewController 및 Cell 파일생성

---
+++ LodgingCommunity/Source/View/ViewControllers/FavoriteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteViewController.swift
+//  LodgingCommunity
+//
+//  Created by 박지현 on 2021/07/02.
+//
+
+import UIKit
+
+class FavoriteViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
